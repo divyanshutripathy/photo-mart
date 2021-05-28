eb68f5db699332c052ded774969bef67b36f1603
divyanshutripathy <divyanshutripathy@jklu.edu.in>
2021-05-28 13:06:51 +0530

    Fourth commit

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,13 +3,15 @@ import db, {auth} from '../../firebase';
 import './Dashboard.css';
 import { Button, GridList, GridListTile } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import dpp from '../../dpp.png';
 
 export default function Dashboard() {
 
     const [user, setUser] = useState({});
     const [data, setData] = useState({});
     const [cate, setCate] = useState({});
-    const [photos, setPhotos] = useState([])
+    const [photos, setPhotos] = useState([]);
+    const [edit, setEdit] = useState(false);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((_user) => {
@@ -34,6 +36,10 @@ export default function Dashboard() {
         return unsubscribe;
     }, [])
 
+    function handleSignOut(){
+        auth.signOut();
+    }
+
     const useStyles = makeStyles((theme) => ({
         root: {
           display: 'flex',
@@ -50,45 +56,65 @@ export default function Dashboard() {
 
     const classes = useStyles();
 
-    return (
-        <div style={{textAlign: "left"}}>
-            <div className="DP">
-                <img src={data.profile} alt="Nothing Here!" height='180vh' width='200vw' style={{borderRadius: '50%'}} />
-                {/* <Avatar alt="Remy Sharp" src={data.profile} sizes="large" /> */}
+    if (!edit){
+        return (
+            <div style={{textAlign: "left"}}>
+                
+                {/* DP Image */}
+                <div className="DP">
+                    <img src={data.profile ? data.profile : dpp} alt="Could not fetch picture" height='180vh' width='200vw' style={{borderRadius: '50%'}} />
+                    {/* <Avatar alt="Remy Sharp" src={data.profile} sizes="large" /> */}
+                </div>
+    
+                {/* Name, email, no., city, & state */}
+                <div className="personal__info">
+                    <h1>{data.Name}</h1>
+                    <p>{data.Email}</p>
+                    <p>{data.No}</p>
+                    <p>{data.City}, {data.State}</p>
+                </div>
+    
+                {/* Edit and Logout Buttons */}
+                <div style={{ display: 'inline', marginLeft: '45vw', verticalAlign: 'top' }} >
+                    <Button color='primary' variant='contained' style={{marginRight: "2vw"}} > Edit </Button>
+                    <Button color='primary' variant='outlinedPrimary' onClick={handleSignOut}> Logout </Button>
+                </div>
+    
+                {/* Categories */}
+                <div className="name"><h2>Categories Selected: </h2></div>
+                <div className="categories">
+                        {
+                            Object.entries(cate).map(([key, value]) => {
+                                if (value){
+                                    return (
+                                        <div className="buttons">
+                                            <Button variant='outlinedPrimary'>{key}</Button>
+                                        </div>
+                                    )
+                                }
+                            })
+                        }
+                </div>
+                
+                <div className="separator">
+                    Your Photos
+                </div>
+    
+                <br/>
+    
+                <div>
+                    {photos.map((tile) => (
+                        <a href={tile} target="_blank"><img src={tile} alt="nhi chal rha" height='200vh' width='280vw' className='photo' /></a>
+                    ))}
+                </div>
+    
             </div>
-            <div className="personal__info">
-                <h1>{data.Name}</h1>
-                <p>{data.Email}</p>
-                <p>{data.No}</p>
-                <p>{data.City}, {data.State}</p>
-            </div>
-            <div className="name"><h2>Categories Selected: </h2></div>
-            <div className="categories">
-                    {
-                        Object.entries(cate).map(([key, value]) => {
-                            if (value){
-                                return (
-                                    <div className="buttons">
-                                        <Button variant='outlinedPrimary'>{key}</Button>
-                                    </div>
-                                )
-                            }
-                        })
-                    }
-            </div>
-            
-            <div className="separator">
-                Your Photos
-            </div>
-
-            <br/>
-
+        )
+    }else{
+        return(
             <div>
-                {photos.map((tile) => (
-                    <img src={tile} alt="nhi chal rha" height='200vh' width='280vw' className='photo' />
-                ))}
+                Sahi hai!
             </div>
-
-        </div>
-    )
+        )
+    }
 }
